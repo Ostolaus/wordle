bb58f60c47c80af7b9fe4f5e03719819f2318003
Nikolaus Benjamin Ostovary <nikolaus.ostovary@student.tugraz.at>
2022-03-09 11:20:00 +0100

    Removed impossible patterns, added printPatterns()

--- wordle.cpp
+++ wordle.cpp
@@ -52,13 +52,38 @@ void readCSV() {
     wordlist = (char**)realloc(wordlist, sizeof(char*)*(wordCount));
 }
 
+void intToBinaryString(size_t val, char* buffer, size_t len){
+    char bin[len+1];
+
+    for (int i = 0; i < len; ++i) {
+        bin[i] = ((val >> (len- 1 - i))&1) ? '1' : '0';
+    }
+
+    bin[len] = 0;
+    memcpy(buffer, bin, len);
+}
+
+void printPatterns(){
+    for (int i = 0; i < patternCount; ++i) {
+        char* fix = (char*)malloc(6);
+        char* ex = (char*)malloc(6);
+        char* non_ex = (char*)malloc(6);
+        assert(fix && ex&& non_ex);
+        intToBinaryString(patterns[i].fixed, fix, 5);
+        intToBinaryString(patterns[i].existing, ex, 5);
+        intToBinaryString(patterns[i].non_existing, non_ex, 5);
+        printf("Fixed:\t\t %s\nExisting:\t %s\nNon_Existing:\t %s\n \n", fix, ex, non_ex);
+    }
+}
+
 void generatePatterns()
 {
     patterns = (Pattern*) malloc(sizeof(Pattern) * (patternCount + 1));
     for (size_t non_existing = 0; non_existing < 0b100000; ++non_existing) {
         for (size_t existing = 0; existing < 0b100000; ++existing) {
             for (size_t fixed = 0; fixed < 0b11111; ++fixed) {
-                if (non_existing + existing + fixed == 0b11111){
+                if (non_existing + existing + fixed == 0b11111
+                    && !(non_existing & existing & fixed)){
                     patterns[patternCount] = Pattern(fixed, existing, non_existing);
                     patternCount++;
                     patterns = (Pattern*) realloc(patterns, sizeof(Pattern) * (patternCount + 1));
@@ -67,9 +92,7 @@ void generatePatterns()
         }
     }
 
-    for (int i = 0; i < patternCount; ++i) {
-        printf("Fixed: %05X, Existing: %05X, Non_Existing: %05X \n",patterns[i].fixed,patterns[i].existing,patterns[i].non_existing );
-    }
+    printPatterns();
 
     printf("Patterncount: %zu\n", patternCount);
 }
